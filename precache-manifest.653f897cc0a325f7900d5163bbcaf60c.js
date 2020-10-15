11051d6cbab60d15b76653583d078bb439f5179d
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-10-15 17:45:17 -0300

    Updates

---
+++ precache-manifest.653f897cc0a325f7900d5163bbcaf60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f8a954ce634e4d5fa907de65ae28af",
+    "url": "/react-alegram/index.html"
+  },
+  {
+    "revision": "1ca0ea5a382e9540cfba",
+    "url": "/react-alegram/static/css/main.8935506f.chunk.css"
+  },
+  {
+    "revision": "0d8338dcd1d87cd19fb4",
+    "url": "/react-alegram/static/js/2.72639b62.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-alegram/static/js/2.72639b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca0ea5a382e9540cfba",
+    "url": "/react-alegram/static/js/main.30b644db.chunk.js"
+  },
+  {
+    "revision": "c7c44f71833ad58ef376",
+    "url": "/react-alegram/static/js/runtime-main.edad23b2.js"
+  }
+]);
